52aa5b329900ecbe5d2dad48fa2a25c184f7c62e
Roshan189 <89222395+Roshan189@users.noreply.github.com>
2023-05-16 14:04:05 +0530

    performs operation like get post delete put using axios

---
+++ src/Read.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+
+const Read = () => {
+  const [data, setData] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/" + id)
+      .then((res) => setData(res.data))
+      .catch((error) => console.log(error));
+  }, [id]);
+
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-light vh-100 w-100">
+      <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
+        <h3>Details of User</h3>
+
+        <div className="mb-2">
+          <strong>Name: {data.name}</strong>
+        </div>
+
+        <div className="mb-2">
+          <strong>Email: {data.email}</strong>
+        </div>
+
+        <div className="mb-2">
+          <strong>Name: {data.phone}</strong>
+        </div>
+
+        <Link to={`/update/${id}`} className="btn btn-success">
+          Edit
+        </Link>
+
+        <Link to="/" className="btn btn-primary ms-3">
+          Back
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Read;
